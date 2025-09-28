20a15f700301e19dc56321f8ad69911e60d3ad47
Simby1 <kimsimby@gmail.com>
2025-09-28 15:48:01 +0100

    feat(setup): Add basic content to Header.tsx for initial check

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 p-4">
+      {/* Placeholder content for Task 1 */}
+      <h1 className="text-white text-lg">App Header</h1>
+    </header>
+  );
+};
+
+export default Header;
